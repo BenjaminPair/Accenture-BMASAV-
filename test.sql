f46014e2d91d612d3e397dfa91458105298e6ece
Aliia Talasbekova <atalasbekova@icstars.org>
2019-01-01 12:02:03 -0600

    create test file to practice

---
+++ test.sql
@@ -0,0 +1,74 @@
+Select *
+ From Customers
+
+Select CustomerID
+ From Customers
+
+Select CustomerID,CustomerName
+ From Customers
+
+ Select ContactName,Address,City
+ From Customers
+
+ Select Country
+ From Customers
+
+ Select CustomerID,CustomerName,Country
+ From Customers
+
+ Select OrderID
+ From Customers
+
+ Select CustomerID
+ From Orders
+
+ Select CustomerID,EmployeeID,OrderDate
+ From Orders
+
+ Select ShipperID,OrderID
+ From Orders
+
+ Select OrderDate,Country
+ From Orders
+
+ Select OrderDetailID
+ From OrderDetails
+
+ Select ShipperName,Phone
+ From Shippers
+
+ Select LastName,FirstName
+ From Customers
+
+ Select LastName,FirstName,Notes
+ From Employees
+
+ Select Birthdate 
+ From Employees
+
+ Select EmployeeID,Photo
+ From Employees
+
+ Select CategoryName,Description
+ From Categories
+
+ Select ProductID,ProductName
+ From Products
+
+ Select SupplierID
+ From Products
+
+ Select UnitPrice
+ From Products
+
+ Select Country
+ From Customers
+
+ Select EmployeeID,CustomerID
+ From Orders
+
+ Select Model,Make,DriveTrain From cars
+ Where DriveTrain='All';
+
+ Select Origin,Type From cars
+ Where Origin='USA';
